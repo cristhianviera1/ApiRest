fcafa9eb4f27e50b36a14663f25cd279f49a8e3f
BatiComputadora <baticomputadora@BatiComputadora.baticueva.com>
2019-01-10 14:13:23 -0500

    se elimina ruta de controlador inexistente

--- app/route/rutas.js
+++ app/route/rutas.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
     // Create a new Customer
     app.get('/api/eventos', controlador.findAll);
     app.get('/api/eventos/:id', controlador.findById);
-    app.post('/api/eventos/addfk', controlador.createFK);
     app.post('/api/eventos/add',controlador.creacionEvento)
     /*app.put('/api/customers', customers.update);*/
 
