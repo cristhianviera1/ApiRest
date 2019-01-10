45d24dc3211df8a9f4f7da814766893fb82bf1ac
BatiComputadora <baticomputadora@BatiComputadora.baticueva.com>
2019-01-10 04:29:47 -0500

    Se elimina funciones innecesarias de controladores

--- app/controller/controlador.js
+++ app/controller/controlador.js
@@ -5,112 +5,13 @@ const Direccion = db.direcciones;
 const Evento = db.eventos;
 const tipoEvento = db.tiposEventos;
 
-/*exports.createFK = (req, res) => {	
-
-	// req = solicitud de ingreso
-	// res = respuesta hacía la petición
-
-	// se exporta funciones para ser consumido por las rutas
-
-	Persona.create({
-				"nombre": req.body.nombre, 
-				"apellido": req.body.apellido
-			})
-			.then(persona => {		
-			for (i=0;i<req.body.numero.length;i++){
-				Telefono.create({
-					"numero":req.body.numero[i],
-					"personaId": persona.id
-				})					
-			};
-			Direccion.create({
-				"sector": req.body.sector,
-				"callePrincipal": req.body.callePrincipal,
-				"calleSecundaria":req.body.calleSecundaria,
-				"nLote":req.body.nLote,
-				"referencia":req.body.referencia,
-				"personaId": persona.id
-			})
-
-			}).then(res.json("Guardado Exitósamente"))
-			.catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-	});		
-};*/
- 
-
-exports.findAll = (req, res) => {
-	
-	Persona.findAll({attributes:['nombre','apellido'],include:[
-		{
-		model: Telefono,
-		attributes:['numero']
-		},{
-			model: Evento,
-			attributes:['fecha','hora'],
-			include:[{
-				model:tipoEvento,
-				attributes:['nombre'],	
-			}]
-		},{
-			model: Direccion,
-			attributes:['sector','callePrincipal','calleSecundaria','nLote','referencia']
-		}
-]}).then(personas => {
-			res.json(personas.sort(function(c1, c2){return c1.id - c2.id}));
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-};
-
-
-
-exports.findById = (req, res) => {	
-	Persona.findById(req.params.id,{attributes:['nombre','apellido'],		
-			include:[
-				{
-					model:Telefono,
-					attributes:['numero']
-				},{
-					model: Direccion,
-					attributes:['sector','callePrincipal','calleSecundaria','nLote','referencia']
-				}
-			]
-		}
-		).then(persona => {
-			res.json(persona);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-}; 
-
-
-	// Actualizar valores de Persona
-exports.update = (req, res) => {
-	const id = req.body.id;
-	Persona.update( req.body, 
-			{ where: {id: id} }).then(() => {
-				res.status(200).json( { mgs: "Updated Successfully -> Persona Id = " + id } );
-			}).catch(err => {
-				console.log(err);
-				res.status(500).json({msg: "error", details: err});
-			});
-};
-
-
-
-
-
 exports.creacionEvento = (req, res) => {	
 
 	// req = solicitud de ingreso
 	// res = respuesta hacía la petición
 
 	// se exporta funciones para ser consumido por las rutas
-	console.log(req.body);
+	
 	Persona.create({
 				"nombre": req.body.nombre, 
 				"apellido": req.body.apellido
@@ -146,4 +47,77 @@ exports.creacionEvento = (req, res) => {
 			console.log(err);
 			res.status(500).json({msg: "error", details: err});
 	});		
+}; 
+
+exports.findAll = (req, res) => {
+	
+	Persona.findAll({attributes:['nombre','apellido'],include:[
+		{
+		model: Telefono,
+		attributes:['numero']
+		},{
+			model: Evento,
+			attributes:['fecha','hora'],
+			include:[{
+				model:tipoEvento,
+				attributes:['nombre'],	
+			}]
+		},{
+			model: Direccion,
+			attributes:['sector','callePrincipal','calleSecundaria','nLote','referencia']
+		}
+]}).then(personas => {
+			res.json(personas.sort(function(c1, c2){return c1.id - c2.id}));
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
 };
+
+
+
+exports.findById = (req, res) => {	
+	Persona.findById(req.params.id,{attributes:['nombre','apellido'],		
+			include:[
+				{
+					model:Telefono,
+					attributes:['numero']
+				},{
+					model: Evento,
+					attributes:['fecha','hora'],
+					include:[{
+						model:tipoEvento,
+						attributes:['nombre'],	
+					}]
+				},{
+					model: Direccion,
+					attributes:['sector','callePrincipal','calleSecundaria','nLote','referencia']
+				}
+			]
+		}
+		).then(persona => {
+			res.json(persona);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
+}; 
+
+
+	// Actualizar valores de Persona
+exports.update = (req, res) => {
+	const id = req.body.id;
+	Persona.update( req.body, 
+			{ where: {id: id} }).then(() => {
+				res.status(200).json( { mgs: "Updated Successfully -> Persona Id = " + id } );
+			}).catch(err => {
+				console.log(err);
+				res.status(500).json({msg: "error", details: err});
+			});
+};
+
+
+
+
+
+
--- app/route/rutas.js
+++ app/route/rutas.js
@@ -2,18 +2,10 @@ module.exports = function(app) {
     const controlador = require('../controller/controlador.js');
  
     // Create a new Customer
-    app.get('/api/personas', controlador.findAll);
-    app.get('/api/personas/:id', controlador.findById);
-    app.post('/api/personas/addfk', controlador.createFK);
-    app.post('/api/personas/add',controlador.creacionEvento)
-/*
-    // Create a new Customer
-    app.post('/api/customers', customers.create);
+    app.get('/api/eventos', controlador.findAll);
+    app.get('/api/eventos/:id', controlador.findById);
+    app.post('/api/eventos/addfk', controlador.createFK);
+    app.post('/api/eventos/add',controlador.creacionEvento)
+    /*app.put('/api/customers', customers.update);*/
 
-     // Update a Customer with Id
-    app.put('/api/customers', customers.update);
- 
-    // Delete a Customer with Id
-    app.delete('/api/customers/:id', customers.delete);
-*/
 }
